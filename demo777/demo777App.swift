e4e0ecc65f8cea72a88c76e0023d483d05786af1
JIN <D1397026@o365.fcu.edu.tw>
2025-07-22 14:36:55 +0800

    Initial Commit

---
+++ demo777/demo777App.swift
@@ -0,0 +1,17 @@
+//
+//  demo777App.swift
+//  demo777
+//
+//  Created by 訪客使用者 on 2025/7/22.
+//
+
+import SwiftUI
+
+@main
+struct demo777App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
